bec999e5f8d165556fc092ddb23a2263e2441ee6
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-22 08:34:15 +0530

    Feature added - delete like

--- src/main/java/com/likeservice/Controller/LikeController.java
+++ src/main/java/com/likeservice/Controller/LikeController.java
@@ -3,6 +3,10 @@ package com.likeservice.Controller;
 
 import com.likeservice.Service.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +14,10 @@ public class LikeController {
 
     @Autowired
     private LikeService likeService;
+
+
+    @DeleteMapping("/postsOrComments/{postOrCommentId}/likes/{likeId}")
+    public ResponseEntity<String> deleteLike(@PathVariable("likeId") String likeId,@PathVariable("postOrCommentId") String postOrCommentId ){
+        return new ResponseEntity<>(likeService.deleteLike(likeId), HttpStatus.ACCEPTED);
+    }
 }
